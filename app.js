6b0fa3cf4374bec67ab2516c72a3b0e6f7b3e263
mohinderps <mohin19derp@gmail.com>
2019-08-01 12:05:22 +0530

    Renders multiple children using m function

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
 var root = document.body;
 
-m.render(root, 'Hello World');
+var whereToRender = root;
+
+var whatToRender = m('main', [
+    m('h1', {class: 'title'}, 'My first app'),
+    m('button', 'A button')
+]);
+
+m.render(root, whatToRender);
